7c4c1303ee2a1eaa9cdd8019075e7fa1ac6a83fb
John Niyontwali <nijohn0006@gmail.com>
2022-06-08 23:32:25 +0200

    Revert "Added the frontend using react"

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv')
 const { graphqlHTTP } = require('express-graphql')
 const colors = require('colors');
-const cors = require('cors');
-
 const connectDB = require('./config/db')
 
 const schema = require('./schema/schema')
@@ -14,7 +12,6 @@ const port = process.env.PORT || 9000
 
 const app = express()
 
-app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development'
